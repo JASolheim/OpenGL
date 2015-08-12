f38b0857c4202e2de7889dc21c73c292bd0acc21
JASolheim <JASolheim@FHSU.edu>
2015-08-12 16:14:14 -0500

    Create Look_At_Vertex_Shader.glsl

---
+++ Look_At_Vertex_Shader.glsl
@@ -0,0 +1,11 @@
+  #version 400
+  layout(location = 0) in vec3 vertexPosition ;
+  layout(location = 1) in vec3 vertexColor ;
+  uniform mat4 modelViewMatrix ;
+  uniform mat4 projectionMatrix ;
+  out vec3 color ;
+  void main ()
+    {
+    gl_Position = projectionMatrix * modelViewMatrix * vec4 ( vertexPosition, 1.0 ) ;
+    color = vertexColor;
+    }
